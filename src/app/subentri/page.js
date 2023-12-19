ab2458acdcc13acdb2ede446c7a582aabaf24ef0
Ciro Moscarella <ciro.moscarella@icloud.com>
2023-12-19 18:30:57 +0100

    update

---
+++ src/app/subentri/page.js
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Subentri from './Subentri'
+
+export default function page() {
+    return (
+      <div>
+            <>
+            <Navbar />
+            <Subentri />
+            </>
+      </div>
+    )
+  }
+  
