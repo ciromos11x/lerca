8647c68c316a32dfcbf4575ea1f60b3e51338593
Ciro Moscarella <ciro.moscarella@icloud.com>
2023-12-27 13:06:29 +0100

    update

--- src/app/autoletture/page.js
+++ src/app/autoletture/page.js
@@ -11,6 +11,7 @@ export default function page() {
       <NavbarAutoletture />
         <Autoletture  />
         <Come />
+        <Footer />
     </div>
   )
 }
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import MailIcon from '../../../public/loghi/bx-mail-send.svg';
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white bg-gradient-to-b from-zinc-400 via-zinc-200 to-zinc-400">
-      <div className="container p-12 flex justify-between">
-
-        
-
-          <div className="flex flex-col justify-end">
-        <p className="text-black mb-2 font-sans">Tutti i diritti riservati.</p>
-        <p className="text-black font-sans">Created By <Link href='https://www.linkedin.com/in/ciro-moscarella-05339024a/'><em>Moci.</em></Link></p>
+    <footer className="footer  z-10   text-white font-sans bg-black py-4 md:py-6">
+      <div className="container p-4 flex flex-col md:flex-row items-center justify-between">
+        <div className="text-center md:text-left mb-4 md:mb-0">
+          <p className=" mb-2 font-sans">Tutti i diritti riservati.</p>
+          <p className=" font-sans">Created By <Link href='https://www.linkedin.com/in/ciro-moscarella-05339024a/'><em>Moci.</em></Link></p>
+        </div>
+        <div className="flex justify-center md:justify-end">
+          {/* Altri elementi o link, se necessario */}
+          
         </div>
       </div>
     </footer>
