0d1aa93ae7418f0c32e6d6fe9c1160804c05ce69
Ciro Moscarella <ciro.moscarella@icloud.com>
2023-12-27 15:23:25 +0100

    update

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,19 +1,32 @@
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import MailIcon from '../../../public/loghi/bx-mail-send.svg';
+// components/Footer.js
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="footer  z-10   text-white font-sans bg-black py-4 md:py-6">
-      <div className="container p-4 flex flex-col md:flex-row items-center justify-between">
-        <div className="text-center md:text-left mb-4 md:mb-0">
-          <p className=" mb-2 font-sans">Tutti i diritti riservati.</p>
-          <p className=" font-sans">Created By <Link href='https://www.linkedin.com/in/ciro-moscarella-05339024a/'><em>Moci.</em></Link></p>
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <div className="flex justify-center items-center">
+        <div className="flex items-center justify-start space-x-4">
+          {/* Icona Email */}
+          <FontAwesomeIcon icon={faEnvelope} size="lg" />
+          {/* Icona LinkedIn */}
+          <Link href='https://it.linkedin.com/in/ciro-moscarella-05339024a' blank>
+          <FontAwesomeIcon icon={faLinkedin} size="lg" />
+          </Link>
         </div>
-        <div className="flex justify-center md:justify-end">
-          {/* Altri elementi o link, se necessario */}
-          
+        <div className="flex-grow text-center ">
+          {/* Inserisci qui il logo del sito */}
+        
+        </div>
+        <div className="flex items-center justify-end space-x-4">
+          {/* Nome e testo copyright */}
+          <div>
+            <p>Created By MoCi</p>
+            <p>&copy; 2023 MoCi.</p>
+          </div>
         </div>
       </div>
     </footer>
