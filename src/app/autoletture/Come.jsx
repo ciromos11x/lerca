0137badb22396d2be70f879215cea5ef4fdfa403
Ciro Moscarella <ciro.moscarella@icloud.com>
2023-12-27 12:57:58 +0100

    update

--- src/app/autoletture/Come.jsx
+++ src/app/autoletture/Come.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Come() {
   return (
-    <section id="come" className=" w-full flex items-center justify-center  md:grid-cols-2 h-screen  bg-black py-24 gap-4">
+    <section id="come" className="text-white w-full flex flex-col items-center justify-center md:flex-row bg-black py-12 md:py-24 gap-4">
+        <div className='text-center md:text-left md:w-1/2 p-6'>
+            <h1 className='text-cyan-400 font-sans font-bold text-4xl mb-4'>Come leggere il contatore dell’acqua</h1>
+            <div className=''>
+                <h1 className='text-xl font-bold font-sans  p-4'>Modello a lancette:</h1>
+                    <p className='text-l pt-2'>I quadranti con numeratori neri indicano i metri cubi, mentre i decimali sono di colore rosso.
+                     Per leggere il contatore basta annotare i numeri con gli indici neri.</p>
+                      La lettura si esegue in senso orario, partendo dal basso a sinistra e procedendo verso destra.
+                      <p className='pb-4'> Devono essere lette solo le lancette degli orologi neri.
+                        Come moltiplicare i valori:</p>
+                        <ul className='p-4'>
+                            <li>Il primo x 1000 m3</li>
+                            <li>Il secondo x 100 m3</li>
+                            <li>Il terzo x 10 m3</li>
+                            <li>Il quarto x 1 m3</li>
+                        </ul>
+                    <p className='text-l pb-4'>Se la lancetta è posta tra due numeri, va considerato il numero più basso che precede la lancetta.</p>
+                    <Image className='p-4' alt='contatore a lancette' src='/image/lancette.png' width={500} height={500} />
+            </div>
 
+            <div className='text-center md:text-left md:w-1/2'>
+                <h1 className='text-xl font-bold font-sans p-4'>Modello a lettura diretta:</h1>
+                <p className='text-l pt-2'>È sufficiente considerare (leggendole come di consueto da sinistra verso destra), le cifre intere di colore nero,
+                 ignorando gli zeri a sinistra del primo numero.</p>
+                 <p className='pb-4'>Se la cifra non è ben allineata, bisogna considerare quella inferiore.</p>
+                 <Image className='p-4' alt='contatore lettura diretta' src='/image/diretta.png' width={500} height={500} />
+            </div>
+        </div>
     </section>
   )
 }
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -12,11 +12,11 @@ const Hero = () => {
   return (
     <div id='home' className='w-full h-screen flex items-center  bg-gradient-to-b from-zinc-400 via-zinc-200 to-zinc-400 m-auto border-slate-300   flex-col justify-between'>
         <div className='grid md:grid-cols-2 mx-auto  max-w-[1240px] bg-gradient-to-b from-zinc-300 via-zinc-200 to-zinc-300 m-auto border-slate-300 rounded-xl  shadow-xl p-12 '>
-            <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
-                <p className='text-2xl'>Lettura & Riparazioni Contatori Acqua</p>
+            <div className='flex flex-col font-sans justify-center md:items-start w-full px-2 py-8'>
+                <p className='text-2xl font-bold'>Lettura & Riparazioni Contatori Acqua</p>
                 <h1 className='py-3 text-5xl md:text-7xl font-bold'><span className='text-cyan-400'>Lerca</span> dal 1948</h1>
                 <h2 className='py-3 text-3xl md:text-4xl font-bold mb-2'>Via Trento 11, Foggia</h2>
-                <p className='text-2xl'>Dall&apos;esperienza del passato una garanzia per il futuro.</p>
+                <p className='text-2xl font-sans'>Dall&apos;esperienza del passato una garanzia per il futuro.</p>
                 <Link className='bg-cyan-400 text-center text-white rounded-xl font-bold py-3 px-6 sm:w-[60%] my-4 hover:bg-cyan-500 transition-all hover:text-2xl ' href='#about'>Scoprici</Link>
 
                 <div className='flex flex-col  py-2 md:min-w-[400px] rounded-xl text-center mt-6 ' >
