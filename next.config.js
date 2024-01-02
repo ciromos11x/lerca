1e5b15f7eebf74fa91ceaf3bffe9940b32f39b53
Ciro Moscarella <ciro.moscarella@icloud.com>
2024-01-02 21:34:52 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ["res.cloudinary.com", "www.w3.org"],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
+  
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
-
+import '../../style/global.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,6 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
