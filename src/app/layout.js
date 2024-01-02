e788fc4982ef5447b1af151697d7fede81200b2f
Ciro Moscarella <ciro.moscarella@icloud.com>
2024-01-02 21:43:05 +0100

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import '../../style/global.css'
+import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +11,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Aggiungi il tag viewport qui */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
