583fcf4b59aefdb3883aa1db2c9dfb2668f00870
Ciro Moscarella <ciro.moscarella@icloud.com>
2024-01-03 13:05:08 +0100

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,22 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from 'next/font/google';
+import Head from 'next/head'; // Importa il componente Head da 'next/head'
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Lerca',
   description: 'Create by Moci.',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         {/* Aggiungi il tag viewport qui */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
